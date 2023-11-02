204a4dd008807bdfa532b485eaaefcca9e853f7f
Erkki Kazonen <erkki.kazonen@gmail.com>
2023-11-02 15:14:10 +0200

    Initial commit

---
+++ src/main/java/com/example/ekintyotuntilista/EkintyotuntilistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ekintyotuntilista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkintyotuntilistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkintyotuntilistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ekintyotuntilista/EkintyotuntilistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ekintyotuntilista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkintyotuntilistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
